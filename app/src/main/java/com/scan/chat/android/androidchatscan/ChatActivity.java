a2299fd79d1368f7eb3927d57bc2c75f8532382b
louis <amoros.louis@gmail.com>
2015-10-13 10:30:03 +0200

    Update method on load message in chat activity.

--- app/src/main/java/com/scan/chat/android/androidchatscan/ChatActivity.java
+++ app/src/main/java/com/scan/chat/android/androidchatscan/ChatActivity.java
@@ -33,6 +33,42 @@ public class ChatActivity extends Activity {
 
         // Call method to load messages with EXTRA_LOGIN
         onLoadMessages();
+
+    }
+
+    class RequestTask extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... uri) {
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpResponse response;
+            String responseString = null;
+            try {
+                response = httpclient.execute(new HttpGet(uri[0]));
+                StatusLine statusLine = response.getStatusLine();
+                if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+                    ByteArrayOutputStream out = new ByteArrayOutputStream();
+                    response.getEntity().writeTo(out);
+                    responseString = out.toString();
+                    out.close();
+                } else{
+                    //Closes the connection.
+                    response.getEntity().getContent().close();
+                    throw new IOException(statusLine.getReasonPhrase());
+                }
+            } catch (ClientProtocolException e) {
+                //TODO Handle problems..
+            } catch (IOException e) {
+                //TODO Handle problems..
+            }
+            return responseString;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            //Do anything with response..
+        }
     }
 
     protected void onLoadMessages() {
@@ -42,40 +78,7 @@ public class ChatActivity extends Activity {
         // showSpinner()
 
         // Request message list
-        class RequestTask extends AsyncTask<String, String, String> {
-
-            @Override
-            protected String doInBackground(String... uri) {
-                HttpClient httpclient = new DefaultHttpClient();
-                HttpResponse response;
-                String responseString = null;
-                try {
-                    response = httpclient.execute(new HttpGet(uri[0]));
-                    StatusLine statusLine = response.getStatusLine();
-                    if(statusLine.getStatusCode() == HttpStatus.SC_OK){
-                        ByteArrayOutputStream out = new ByteArrayOutputStream();
-                        response.getEntity().writeTo(out);
-                        responseString = out.toString();
-                        out.close();
-                    } else{
-                        //Closes the connection.
-                        response.getEntity().getContent().close();
-                        throw new IOException(statusLine.getReasonPhrase());
-                    }
-                } catch (ClientProtocolException e) {
-                    //TODO Handle problems..
-                } catch (IOException e) {
-                    //TODO Handle problems..
-                }
-                return responseString;
-            }
-
-            @Override
-            protected void onPostExecute(String result) {
-                super.onPostExecute(result);
-                //Do anything with response..
-            }
-        }
+        new RequestTask().execute("http://training.loicortola.com/chat-rest/2.0");
         // >>>>>>>>
 
         // <<<<<<<<
