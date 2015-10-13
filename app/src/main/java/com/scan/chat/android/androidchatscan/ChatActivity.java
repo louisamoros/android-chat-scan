23ef2529f5cb117c9fdb3020ddd1820e00e40a43
louis <amoros.louis@gmail.com>
2015-10-13 08:18:11 +0200

    Stash pop on activity chat view with send message button.

--- app/src/main/java/com/scan/chat/android/androidchatscan/ChatActivity.java
+++ app/src/main/java/com/scan/chat/android/androidchatscan/ChatActivity.java
@@ -1,16 +1,35 @@
 package com.scan.chat.android.androidchatscan;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.DefaultHttpClient;
 
 public class ChatActivity extends Activity {
+
+    // UI references.
+    private EditText mMessageText;
+    private Button mSendButton;
+    private View mProgressView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        onLoadMessages();
     }
 
     @Override
@@ -34,5 +53,53 @@ public class ChatActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    protected void onLoadMessages() {
+        //TODO: Load messages from discussion id:X.
+        HttpUriRequest request = new HttpGet("http://training.loicortola.com/chat-rest/messages?&limit=10&offset=20");
+        // Or HttpPost(), depends on your needs
+        String credentials = "louis:amoros";
+        String base64EncodedCredentials = Base64.encodeToString(credentials.getBytes(), Base64.NO_WRAP);
+        request.addHeader("Authorization", "Basic " + base64EncodedCredentials);
+
+        HttpClient httpclient = new DefaultHttpClient();
+//        httpclient.execute(request);
+        // You'll need to handle the exceptions thrown by execute()
+    }
+
+    protected void onSendMessage() {
+        //TODO: Post message in current discussion (id discussion)
+    }
+
+    protected void sendingMessage() {
+        //TODO: Spinner is spinning until we receive status 200.
+    }
+
+
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+        }
     }
 }
