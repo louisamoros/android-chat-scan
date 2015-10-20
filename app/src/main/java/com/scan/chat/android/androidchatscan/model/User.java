965af3c60069a72924518cadacbf3e70b3d3a0cb
Guillaume Nostrenoff <g.nostrenoff@gmail.com>
2015-10-20 15:26:31 +0200

    new class User created.

---
+++ app/src/main/java/com/scan/chat/android/androidchatscan/model/User.java
@@ -0,0 +1,51 @@
+package com.scan.chat.android.androidchatscan.model;
+
+import android.util.Base64;
+
+/**
+ * Created by guillaumenostrenoff on 20/10/15.
+ */
+public class User {
+
+    private String login;
+    private String password;
+
+    public User(String username, String password)
+    {
+        this.login = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+        this.login = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    /**
+     *
+     * @return auth
+     */
+    public String getEncodedBase64()
+    {
+        if(login == null || password == null)
+            return null;
+
+        String credentials = new StringBuilder(login + ":" + password).toString();
+        return "Basic " + Base64.encodeToString(credentials.getBytes(), Base64.NO_WRAP);
+
+    }
+
+
+}
